01283cfec0f2b5a437af279eb70a1b940661ac22
VM <arimas@foxmail.com>
2023-10-17 12:12:27 +0800

    feat: add coingecko api path constants

---
+++ coingecko/const.go
@@ -0,0 +1,98 @@
+package coingecko
+
+// define public and pro api endpoint
+const (
+	publicAPIEndpoint = "https://api.coingecko.com/api/v3"
+	proAPIEndpoint    = "https://pro-api.coingecko.com/api/v3"
+)
+
+const (
+	applicationJSONHeader = "application/json"
+)
+
+// CoinGecko API path
+// both public and paid can use these paths
+const (
+	// ping
+	pingPath = "/ping"
+
+	// simple
+	simplePricePath           = "/simple/price"
+	simpleTokenPricePath      = "/simple/token_price/%s"
+	supportedVsCurrenciesPath = "/simple/supported_vs_currencies"
+
+	// coins
+	coinsListPath             = "/coins/list"
+	coinsMarkets              = "/coins/markets"
+	coinsIDPath               = "/coins/%s"
+	coinsTickersPath          = "/coins/%s/tickers"
+	coinsHistoryPath          = "/coins/%s/history"
+	coinsMarketChartPath      = "/coins/%s/market_chart"
+	coinsMarketChartRangePath = "/coins/%s/market_chart/range"
+	coinsOHLCPath             = "/coins/%s/ohlc"
+
+	// contract
+	coinsContractPath                 = "/coins/%s/contract/%s"
+	coinsContractMarketChartPath      = "/coins/%s/contract/%s/market_contract/"
+	coinsContractMarketChartRangePath = "/coins/%s/contract/%s/market_contract/range"
+
+	// asset platforms
+	assetPlatformsPath = "/asset_platforms"
+
+	// categories
+	categoriesListPath = "/coins/categories/list"
+	categoriesPath     = "/coins/categories"
+
+	// exchanges
+	exchangesPath            = "/exchanges"
+	exchangesListPath        = "/exchanges/list"
+	exchangesIDPath          = "/exchanges/%s"
+	exchangesTickerPath      = "/exchanges/%s/tickers"
+	exchangesVolumeChartPath = "/exchanges/%s/volume_chart"
+
+	// derivatives
+	derivativesPath          = "/derivatives"
+	derivativesExchangesPath = "/derivatives/exchanges"
+	derivativesIDPath        = "/derivatives/exchanges/%s"
+	derivativesListPath      = "/derivatives/exchanges/list"
+
+	// nfts
+	nftsListPath     = "/nfts/list"
+	nftsIDPath       = "/nfts/%s"
+	nftsContractPath = "/nfts/%s/contract/%s"
+
+	// exchange rates
+	exchangeRatesPath = "/exchange_rates"
+
+	// search
+	searchPath = "/search"
+
+	// trending
+	trendingPath = "/search/trending"
+
+	// global
+	globalPath = "/global"
+	globalDefi = "/global/decentralized_finance_defi"
+
+	// companies
+	companiesPath = "/companies/public_treasury/%s"
+)
+
+// paid plan apis
+const (
+	coinsListNewPath             = "/coins/list/new"
+	topGainersLoserPath          = "/coins/top_gainers_losers"
+	globalMarketCapChartPath     = "/global/market_cap_chart"
+	nftsMarketPath               = "/nfts/markets"
+	nftsMarketChartPath          = "/nfts/%s/market_chart"
+	nftsContractMarketChartPath  = "/nfts/%s/contract/%s/market_chart"
+	nftsTickersPath              = "/nfts/%s/tickers"
+	exchangeVolumeChartRangePath = "/exchange/%s/volume_chart/range"
+)
+
+// enterprise plan apis
+const (
+	coinsCirculatingSupplyChartPath      = "/coins/%s/circulating_supply_chart"
+	coinsCirculatingSupplyChartRangePath = "/coins/%s/circulating_supply_chart/range"
+	tokenListAllPath                     = "/token_lists/%s/all.json"
+)
---
+++ coingecko/model.go
@@ -0,0 +1,6 @@
+package coingecko
+
+// PingResponse
+type PingResponse struct {
+	GeckoSays string `json:"gecko_says"`
+}
