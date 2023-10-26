331e85054ce770f149d727557ebd79d6f79e7116
VM <arimas@foxmail.com>
2023-10-26 16:52:39 +0800

    feat: add GeckoTerminal token and ohlcv apis

--- geckoterminal/types.go
+++ geckoterminal/types.go
@@ -45,6 +45,7 @@ type PoolAttributesItem struct {
 	Address                       string                      `json:"address"`
 	Name                          string                      `json:"name"`
 	PoolCreatedAt                 *time.Time                  `json:"pool_created_at"`
+	TokenPriceUSD                 *string                     `json:"token_price_usd,omitempty"`
 	FDVUsed                       *string                     `json:"fdv_used"`
 	MarketCapUSD                  *string                     `json:"market_cap_usd"`
 	PriceChangePercentage         map[string]string           `json:"price_change_percentage"`
@@ -66,6 +67,62 @@ type PoolIncludedItem struct {
 		Address         string  `json:"address,omitempty"`
 		Name            string  `json:"name"`
 		Symbol          string  `json:"symbol,omitempty"`
+		CoingeckoCoinID *string `json:"coingecko_coin_id,omitempty"`
+	} `json:"attributes"`
+}
+
+// TokenDataItem
+type TokenDataItem struct {
+	basicStruct
+	Attributes    TokenAttributesItem `json:"attributes"`
+	Relationships struct {
+		TopPools map[string][]basicStruct `json:"top_pools"`
+	} `json:"relationships"`
+}
+
+// TokenAttributesItem
+type TokenAttributesItem struct {
+	Address           string            `json:"address"`
+	Name              string            `json:"name"`
+	Symbol            string            `json:"symbol"`
+	CoingeckoCoinID   string            `json:"coingecko_coin_id"`
+	Decimals          int               `json:"decimals"`
+	TotalSupply       string            `json:"total_supply"`
+	PriceUSD          string            `json:"price_usd"`
+	FDVUSD            string            `json:"fdv_usd"`
+	TotalReserveInUSD string            `json:"total_reserve_in_usd"`
+	VolumeUSD         map[string]string `json:"volume_usd"`
+	MarketCapUSD      *string           `json:"market_cap_usd"`
+}
+
+// TokenInfoDataItem
+type TokenInfoDataItem struct {
+	basicStruct
+	Attributes    TokenInfoAttributesItem `json:"attributes"`
+	Relationships map[string]basicStruct  `json:"relationships"`
+}
+
+// TokenInfoAttributesItem
+type TokenInfoAttributesItem struct {
+	Address           string    `json:"address"`
+	Name              string    `json:"name"`
+	Symbol            string    `json:"symbol"`
+	CoingeckoCoinID   string    `json:"coingecko_coin_id"`
+	ImageURL          string    `json:"image_url"`
+	Websites          []string  `json:"websites"`
+	Description       string    `json:"description"`
+	GTScore           float64   `json:"gt_score"`
+	DiscordURL        *string   `json:"discord_url"`
+	TelegramHandle    *string   `json:"telegram_handle"`
+	TwitterHandle     *string   `json:"twitter_handle"`
+	MetadataUpdatedAt time.Time `json:"metadata_updated_at,omitempty"`
+}
+
+// TokensInfoIncludedItem
+type TokensInfoIncludedItem struct {
+	basicStruct
+	Attributes struct {
+		Name            string  `json:"name"`
 		CoingeckoCoinID *string `json:"coingecko_coin_id,omitempty"`
 	} `json:"attributes"`
 }
