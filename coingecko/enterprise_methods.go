0cd0cc50a4afca5466c573116b4c2fe1c05443ca
VM <arimas@foxmail.com>
2023-11-02 14:41:54 +0800

    fix: optimize CoinGecko method names

--- coingecko/enterprise_methods.go
+++ coingecko/enterprise_methods.go
@@ -9,11 +9,11 @@ import (
 	"strconv"
 )
 
-// CoinsIDCirculatingSupplyChart gets historical circulating supply of a coin, by number of days away from now.
+// GetCirculatingSupplyChartByCoinID gets historical circulating supply of a coin, by number of days away from now.
 //
 // CoinGecko equivalent page: https://www.coingecko.com/en/exchanges/binance#statistics (Exchange Trade Volume chart)
 //
-// Data Granularity (auto): 1 day from now = 5-minutely data. 2-90 days from now = hourly data. 91 days & above
+// Data Granularity (auto): 1 day from now = 5-minutely data; 2-90 days from now = hourly data; 91 days & above
 // from now = daily data (00:00 UTC).
 //
 // Data Availability: from 22 June 2019.
@@ -30,13 +30,14 @@ import (
 // days(required): data up to number of days ago. Valid values: any integer, e.g. 1, 14, 30 ...
 //
 // interval(optional): data interval. Valid values: daily. if interval is not specified, auto data granularity will apply.
-func (c *Client) CoinsIDCirculatingSupplyChart(ctx context.Context, id string, days int, interval string) (*CoinsIDCirculatingSupplyChartResponse, error) {
+func (c *Client) GetCirculatingSupplyChartByCoinID(ctx context.Context, id string, days uint, interval string) (
+	*CoinsIDCirculatingSupplyChartResponse, error) {
 	if id == "" {
-		return nil, fmt.Errorf("id should not be empty")
+		return nil, fmt.Errorf("coin id should not be empty")
 	}
 
 	params := url.Values{}
-	params.Add("days", strconv.Itoa(days))
+	params.Add("days", strconv.Itoa(int(days)))
 	if interval != "" {
 		params.Add("interval", interval)
 	}
@@ -57,11 +58,11 @@ func (c *Client) CoinsIDCirculatingSupplyChart(ctx context.Context, id string, d
 	return &data, nil
 }
 
-// CoinsIDCirculatingSupplyChartRange gets historical circulating supply of a coin, within a range of timestamp.
+// GetCirculatingSupplyChartRangeByCoinID gets historical circulating supply of a coin, within a range of timestamp.
 //
 // CoinGecko equivalent page: https://www.coingecko.com/en/exchanges/binance#statistics (Exchange Trade Volume chart)
 //
-// Data Granularity (auto): 1 day from now = 5-minutely data. 2-90 days from now = hourly data. 91 days & above
+// Data Granularity (auto): 1 day from now = 5-minutely data; 2-90 days from now = hourly data; 91 days & above
 // from now = daily data (00:00 UTC).
 //
 // Data Availability: from 22 June 2019.
@@ -78,9 +79,10 @@ func (c *Client) CoinsIDCirculatingSupplyChart(ctx context.Context, id string, d
 // from(required): from date in UNIX Timestamp. Valid values: UNIX timestamp, e.g. 1633046400.
 //
 // to(required): to date in UNIX Timestamp. Valid values: UNIX timestamp, e.g. 1635724799.
-func (c *Client) CoinsIDCirculatingSupplyChartRange(ctx context.Context, id string, from, to int64) (*CoinsIDCirculatingSupplyChartResponse, error) {
+func (c *Client) GetCirculatingSupplyChartRangeByCoinID(ctx context.Context, id string, from, to int64) (
+	*CoinsIDCirculatingSupplyChartResponse, error) {
 	if id == "" {
-		return nil, fmt.Errorf("id should not be empty")
+		return nil, fmt.Errorf("coin id should not be empty")
 	}
 
 	params := url.Values{}
@@ -103,7 +105,7 @@ func (c *Client) CoinsIDCirculatingSupplyChartRange(ctx context.Context, id stri
 	return &data, nil
 }
 
-// TokensListAll gets full list of tokens of a blockchain network (asset platform) that is supported by Ethereum
+// ListAllTokensByAssetPlatformID gets full list of tokens of a blockchain network (asset platform) that is supported by Ethereum
 // token list standard(https://tokenlists.org/).
 //
 // Update Frequency: 5 minutes.
@@ -113,8 +115,8 @@ func (c *Client) CoinsIDCirculatingSupplyChartRange(ctx context.Context, id stri
 // Path parameters:
 //
 // asset_platform_id(required): this is path to pass the asset platform id, not query parameter. Valid values: any
-// asset platform id, e.g. polygon-pos , arbitrum-nova, ethereum ...
-func (c *Client) TokensListAll(ctx context.Context, assetPlatformID string) (*TokensListAllResponse, error) {
+// asset platform id, e.g. polygon-pos, arbitrum-nova, ethereum ...
+func (c *Client) ListAllTokensByAssetPlatformID(ctx context.Context, assetPlatformID string) (*TokensListAllResponse, error) {
 	if assetPlatformID == "" {
 		return nil, fmt.Errorf("asset_platform_id should not be empty")
 	}
@@ -123,7 +125,7 @@ func (c *Client) TokensListAll(ctx context.Context, assetPlatformID string) (*To
 	endpoint := fmt.Sprintf("%s%s", c.apiURL, path)
 	resp, _, err := c.sendReq(ctx, endpoint)
 	if err != nil {
-		slog.Error("failed to send request to tokens list all api", "error", err)
+		slog.Error("failed to send request to list all tokens api", "error", err)
 		return nil, err
 	}
 
